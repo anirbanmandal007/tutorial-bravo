deee5474b8f5a14c871b40343be0ea7c4ddc88cb
Atit Patel <atitpatel@Atits-MacBook-Air.local>
2020-04-25 18:14:13 -0400

    new component created

---
+++ src/app/topics/objectmanipulation/objectmanipulation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-objectmanipulation',
+  templateUrl: './objectmanipulation.component.html',
+  styleUrls: ['./objectmanipulation.component.scss']
+})
+export class ObjectmanipulationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
